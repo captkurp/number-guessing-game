05cb941140eb02fc96667d00ab1215476ebf9f57
cptkurp <jberryit91@gmail.com>
2024-08-28 18:03:36 +0200

    Added comments

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,22 +1,28 @@
 import random
+#import random to generate a number
 
+
+#main function includes the welcome message and the main logic loop, the variable "win_con" detects if the player has guessed the number
 def main():
     welcome_message()
     repeat = True
     while repeat == True:
         win_con = 0
         number = generate_number()
-        num_of_guesses = difficulty_select()
-        while num_of_guesses >= 0 and win_con == 0:
+        guesses_remaining = difficulty_select()
+        #this while loop allows the user to continue guessing until they either run out of guesses or satisfy the win condition
+        while guesses_remaining >= 0 and win_con == 0:
             user_num = prompt_user()
             win_con = evaluate_guess(number, user_num, win_con)
-            print(f"main wincon {win_con}")
-            print_guesses(num_of_guesses)
-            num_of_guesses -= 1
             if win_con == 1:
-                print("          Great job you won with {num_of_guesses} guesses left!")
+                #this logic is only run once the win_con is met, the continue at the end of the run kicks the loop back up to evaluate line 14, and finds that line 14 is no longer true, thus moving to line 23 to prompt for a repeat
+                print(f"          Great job you won with {guesses_remaining} guesses left!")
+                continue
+            print_guesses(guesses_remaining)
+            guesses_remaining -= 1
         repeat = play_again()
 
+#generate the number between 1 and 100
 def generate_number():
     return random.randint(1, 100)
 
@@ -36,6 +42,7 @@ def difficulty_select():
     print("          2. Medium (5 guesses)")
     print("          3. Hard (3 guesses)\n")
     difficulty = input("          Please select a difficulty: ")
+    #loop to determine difficulty chosen, since the loop in main is >= 0, the return is the number of guesses the user gets after their initial guess, to allow a user to guess even with 0 guesses remaining, variable was renamed to guesses_remaining for clarity
     while True:
         if difficulty == "1":
             return 9
@@ -46,21 +53,21 @@ def difficulty_select():
         else:
             difficulty = input("          Invalid option, please select a difficulty: ")
 
+#evaluate function to check if the users number was too high or too low, returns the win win con
 def evaluate_guess(number, user_num, win_con):
     if user_num > number:
-        print(f"          Too high, the number is less than {user_num}!")
+        print(f"          Too high, the number is less than {user_num}!\n")
         return win_con
     elif user_num < number:
-        print(f"          Too low, the number is greater than {user_num}!")
+        print(f"          Too low, the number is greater than {user_num}!\n")
         return win_con
     else:
-        print("runnign")
         win_con = 1
         return win_con
 
 def play_again():
     while True:
-        choice = input("           Would you like to play again? Y or N :")
+        choice = input("          Would you like to play again? Y or N :")
         if choice == 'y':
             return True
         elif choice == 'n':
@@ -68,8 +75,8 @@ def play_again():
         else:
             print("          Unrecognized input, please try again")
 
-def print_guesses(num_of_guesses):
-    print(f"          You have {num_of_guesses} guesses left!")
+def print_guesses(guesses_remaining):
+    print(f"          You have {guesses_remaining} guesses left!\n")
 
 if __name__ == "__main__":
     main()
